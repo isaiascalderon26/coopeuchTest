07d383d4fb9f8f256593a6b03f498c66ffc0cb7e
isaias calderon <isaiasa.calderon@gmail.com>
2023-07-24 18:31:30 -0400

    new project

---
+++ src/main/java/com/desafio/app/rest/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.desafio.app.rest.config;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
